ead1f40120c06714317f463afcb694ddb55a204a
Beth Hansen and Joe Werderber <joe@>
2017-04-24 14:56:03 -0700

    added js,HTML

--- js/scripts.js
+++ js/scripts.js
@@ -7,3 +7,35 @@ var number1 = parseInt(prompt("enter a number:"));
 var number2 = parseInt(prompt("enter another number:"));
 
 alert(add(number1, number2));
+
+var add = function(number1,number2){
+	return number1 * number2;
+};
+
+
+var number1 = parseInt(prompt("enter a number:"));
+var number2 = parseInt(prompt("enter another number:"));
+
+alert(add(number1, number2));
+
+var add = function(number1,number2){
+	return number1 - number2;
+};
+
+
+var number1 = parseInt(prompt("enter a number:"));
+var number2 = parseInt(prompt("enter another number:"));
+
+alert(add(number1, number2));
+
+var add = function(number1,number2){
+	return number1 / number2;
+};
+
+
+var number1 = parseInt(prompt("enter a number:"));
+var number2 = parseInt(prompt("enter another number:"));
+var result = add(number1, number2);
+alert(result);
+
+alert(add(number1, number2));
